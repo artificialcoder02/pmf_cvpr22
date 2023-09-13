58c56d8fb040ef1e93e70311f55dbb495f1372d0
ArtificialCoder-02 <rc.tuhin02@gmail.com>
2023-09-14 00:46:26 +0530

    Added Support for AID dataset

---
+++ datasets/aerialid.py
@@ -0,0 +1,35 @@
+import numpy as np
+import torchvision.transforms as transforms
+
+def dataset_setting(nSupport, img_size=600):
+    """
+    Return EuroSAT dataset setting
+
+    :param int nSupport: number of support examples
+    """
+    mean = [0.415, 0.456, 0.462]  # ImageNet mean values
+    std = [0.197, 0.209, 0.237]  # ImageNet standard deviation values
+    normalize = transforms.Normalize(mean=mean, std=std)
+    
+    trainTransform = transforms.Compose([
+        transforms.RandomResizedCrop((img_size, img_size), scale=(0.8, 1.0)),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ])
+    
+    valTransform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        normalize
+    ])
+    
+    inputW, inputH, nbCls = img_size, img_size, 10  # Assuming EuroSAT has 10 classes
+    
+    trainDir = r"C:\Users\rctuh\Desktop\ISRO\pmf_cvpr22\data\EURO_SPLIT\train"
+    valDir = r"C:\Users\rctuh\Desktop\ISRO\pmf_cvpr22\data\EURO_SPLIT\val"
+    testDir = r"C:\Users\rctuh\Desktop\ISRO\pmf_cvpr22\data\EURO_SPLIT\test"
+    episodeJson = r"C:\Users\rctuh\Desktop\ISRO\pmf_cvpr22\data\EURO_SPLIT\5way5shot_data.json"  # Replace with actual path
+    
+    return trainTransform, valTransform, inputW, inputH, trainDir, valDir, testDir, episodeJson, nbCls
